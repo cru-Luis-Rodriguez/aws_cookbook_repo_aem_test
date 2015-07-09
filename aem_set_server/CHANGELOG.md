2ec9c5d444e77793546e96b9dd5a6fed3124e208
luis rodriguez <luis.rodriguez@cru.org>
2015-07-09 14:41:06 -0400

    add setup server

---
+++ aem_set_server/CHANGELOG.md
@@ -0,0 +1,13 @@
+aem_set_server CHANGELOG
+========================
+
+This file is used to list changes made in each version of the aem_set_server cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of aem_set_server
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ aem_set_server/recipes/default.rb
@@ -0,0 +1,77 @@
+#
+# Cookbook Name:: aem_set_server
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "apache2"
+
+case node:[platform]
+  when "ubuntu", "debian"
+    execute 'apt-get-update' do
+      command 'apt-get update'
+      ignore_failure true
+      not_if { ::File.exist?('/var/lib/apt/periodic/update-success-stamp') }
+    end
+  when "redhat", "centor"
+      execute 'yum-update' do
+        command 'yum update'
+        ignore_failure true
+    end
+  end
+
+
+package "curl" do
+  action :install
+end
+
+case node[:platform]
+  when "redhat", "centos"
+    package "libcurl-devel" do
+      action :upgrade
+    end
+  when "ubuntu", "debian"
+   %w{libcurl4-gnutls-dev libcurl4-openssl-dev}.each do |pkg1|
+     package "#{pkg1}" do
+       action :upgrade
+      end
+    end
+end
+
+%w{maven ruby git gcc}.each do |pkg2|
+ package "#{pkg2}" do
+   action :upgrade
+ end
+end
+
+gem_package "bundler" do
+  action :install
+  ignore_failure true
+end
+
+case node[:platform]
+  when "redhat", "centos"
+    chef_gem "curb" do
+      compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+      action :nothing
+    end.run_action(:install)
+  when "ubuntu", "debian"
+    chef_gem "curb" do
+      compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+      action :install
+      ignore_failure true
+    end
+end
+
+case node:[platform]
+  when "ubuntu", "debian"
+    directory "#{node[:apache][:dir]}/conf" do
+      owner "root"
+      group node[:apache][:root_group]
+      mode "0775"
+      action :create
+      only_if { ::File.directory?('#{node[:apache][:dir]}') }
+    end
+  end
