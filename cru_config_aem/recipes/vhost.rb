9d3b9f91a5ea52a3736b73cd649d75d5b4746070
luis rodriguez <luis.rodriguez@cru.org>
2015-07-24 16:10:01 -0400

    fix issue with stack setting variable declaration

--- cru_config_aem/recipes/vhost.rb
+++ cru_config_aem/recipes/vhost.rb
@@ -4,11 +4,9 @@
 # Copyright 2014, cru
 #
 # All rights reserved - Do Not Redistribute
-#
-#include_recipe 'apt'
-#include_recipe 'apache2'
 
-service 'apache2' do
+
+service 'httpd' do
     supports :status => true, :restart => true
     provider Chef::Provider::Service::Init
 end
@@ -20,12 +18,12 @@ template "#{node['aem']['apache']['home']}/sites-available/cru.conf" do
     mode 0644
     variables(
               :host_name => node['hostname'],
-              :host_ip => node['ipaddress'],
-              :site => node['dispatcher']['site_name'],
+              :host_ip => node['ipaddress']},
+              :site => "#{node['dispatcher']['site_name']}",
               :site_alias1 => node['dispatcher']['alias1'],
               :site_alias2 => node['dispatcher']['alias2'],
-	            :server_admin => node['dispatcher']['vhost_email'],
-              :host_alias => "node['host_name'].cru.org"
+	            :server_admin => "#{node['dispatcher']['vhost_email']}",
+              :host_alias => "#{node['host_name']}.cru.org"
               )
 
 	only_if { File.exist?("#{node['aem']['apache']['home']}/sites-available") }
