291d616985be8503e7c0ffffd4d9d8962b68f611
luis rodriguez <luis.rodriguez@cru.org>
2015-07-28 15:18:14 -0400

    removed extra author and publisher attributes only needed port #

--- cru_config_aem/attributes/defaults.rb
+++ cru_config_aem/attributes/defaults.rb
@@ -5,25 +5,5 @@ default['aem']['dispatcher']['alias1'] = "some_site_alias"
 default['aem']['dispatcher']['alias2'] = "some_site_alias"
 default['aem']['dispatcher']['vhost_email'] = "some_email@domain"
 default['aem']['dispatcher']['domain'] = "nil"
-
-default[:aem][:author] = {
-  :default_context => "/opt/aem/author",
-  :port => "4502",
-  :runnable_jar => "aem-author-p4502.jar",
-  :base_dir => "/opt/aem/author/crx-quickstart",
-  :jvm_opts => {},
-  :ldap => {
-    :enabled => false,
-    :options => {}
-  },
-
-  default[:aem][:publish] = {
-    :default_context => "/opt/aem/publish",
-    :port => "4503",
-    :runnable_jar => "aem-publish-p4503.jar",
-    :base_dir => "/opt/aem/publish/crx-quickstart",
-    :jvm_opts => {},
-    :ldap => {
-      :enabled => false,
-      :options => {}
-    },
+default['aem']['author']['port'] = "4502"
+default['aem']['publish']['port'] = "4503"
