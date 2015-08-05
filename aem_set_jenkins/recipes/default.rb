4c091081086b5ffbc03ef6d21c9bedc48a0decdc
luis rodriguez <luis.rodriguez@cru.org>
2015-08-05 13:06:32 -0400

    create a list of jobs, use list to dowload job config from S3

--- aem_set_jenkins/recipes/default.rb
+++ aem_set_jenkins/recipes/default.rb
@@ -21,49 +21,52 @@ directory "/tmp/jobs_config" do
     mode "0755"
     action :create
 end
-
-aws_s3_file "/tmp/jobs_config/cruorgaem6.xml" do
-    bucket "cru-aem6"
-    remote_path ("/installation_files/jenkins/jobs_config/cruorgaem6.xml")
-    aws_access_key_id aws['aws_access_key_id']
-    aws_secret_access_key aws['aws_secret_access_key']
-    mode "0644"
-    not_if { ::File.exist?("/tmp/jobs_config/cruorgaem6.xml") }
+jobs = [cruorgaem6,Backup_AEM_Production_Author_Repo,CruOrgaem6_Auto_Production_DailyContentFlush,CruOrgaem6_Manual_Prod_Pub_Designs_DispatcherClear]
+jobs.sort! { |a,b| a[:name] <=> b[:name] }
+jobs.each do |j|
+  aws_s3_file "/tmp/jobs_config/#{j}.xml" do
+      bucket "cru-aem6"
+      remote_path ("/installation_files/jenkins/jobs_config/#{j}.xml")
+      aws_access_key_id aws['aws_access_key_id']
+      aws_secret_access_key aws['aws_secret_access_key']
+      mode "0644"
+      not_if { ::File.exist?("/tmp/jobs_config/#{j}.xml") }
+  end
 end
-
-jenkins_plugin 'greenballs'
-#jenkins_plugin 'disk-usage plugin'
-#jenkins_plugin 'AntPlugin'
-#jenkins_plugin 'CASPlugin'
-#jenkins_plugin 'CASprotocolversion1plugin'
-#jenkins_plugin 'CredentialsPlugin'
-#jenkins_plugin 'CVSPlug-in'
-#jenkins_plugin 'ExternalMonitorJobTypePlugin'
-#jenkins_plugin 'GITclientplugin'
-#jenkins_plugin 'GITplugin'
-#jenkins_plugin 'HipChatPlugin'
-#jenkins_plugin 'JavadocPlugin'
-#jenkins_plugin 'JUnitPlugin'
-#jenkins_plugin 'LDAPPlugin'
-#jenkins_plugin 'MailerPlugin'
-#jenkins_plugin 'MapDBAPIPlugin'
-#jenkins_plugin 'MatrixAuthorizationStrategyPlugin'
-#jenkins_plugin 'MatrixProjectPlugin'
-#jenkins_plugin 'MavenInfoPlugin'
-#jenkins_plugin 'MavenIntegrationplugin'
-#jenkins_plugin 'MetricsDiskUsagePlugin'
-#jenkins_plugin 'MetricsPlugin'
-#jenkins_plugin 'OWASPMarkupFormatterPlugin'
-#jenkins_plugin 'PAMAuthenticationplugin'
-#jenkins_plugin 'SCMAPIPlugin'
-#jenkins_plugin 'ScriptSecurityPlugin'
-#jenkins_plugin 'SlackNotificationPlugin'
-#jenkins_plugin 'SSHCredentialsPlugin'
-#jenkins_plugin 'SSHSlavesplugin'
-#jenkins_plugin 'SubversionPlug-in'
-#jenkins_plugin 'TokenMacroPlugin'
-#jenkins_plugin 'TranslationAssistanceplugin'
-#jenkins_plugin 'WindowsSlavesPlugin'
+#list of plugin to install
+#use the plugin id -- not plugin name
+jenkins_plugin 'disk-usage'
+jenkins_plugin 'ant'
+jenkins_plugin 'cas-plugin'
+jenkins_plugin 'cas1'
+jenkins_plugin 'credentials'
+jenkins_plugin 'cvs'
+jenkins_plugin 'external-monitor-job'
+jenkins_plugin 'git-client'
+jenkins_plugin 'git'
+jenkins_plugin 'hipchat'
+jenkins_plugin 'javadoc'
+jenkins_plugin 'junit'
+jenkins_plugin 'ldap'
+jenkins_plugin 'mailer'
+jenkins_plugin 'mapdb-api'
+jenkins_plugin 'matrix-auth'
+jenkins_plugin 'matrix-project'
+jenkins_plugin 'maven-info'
+jenkins_plugin 'maven-plugin'
+jenkins_plugin 'metrics-diskusage'
+jenkins_plugin 'metrics'
+jenkins_plugin 'antisamy-markup-formatter'
+jenkins_plugin 'pam-auth'
+jenkins_plugin 'scm-api'
+jenkins_plugin 'script-security'
+jenkins_plugin 'slack'
+jenkins_plugin 'ssh-credentials'
+jenkins_plugin 'ssh-slaves'
+jenkins_plugin 'subversion'
+jenkins_plugin 'token-macro'
+jenkins_plugin 'translation'
+jenkins_plugin 'windows-slaves'
 
 # Create a jenkins job (default action is `:create`)
 jenkins_job 'cruorgaem6' do
